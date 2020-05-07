0f356cf86ec112dd93ee66b556335d150125da22
Uri <47351041+Gelelus@users.noreply.github.com>
2020-05-07 15:33:52 +0300

    little cleaning

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -8,7 +8,7 @@ import {
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
-import { map, tap } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
@@ -18,20 +18,13 @@ export class AuthGuard implements CanActivate {
     router: RouterStateSnapshot
   ): boolean | Promise<boolean | UrlTree> | Observable<boolean | UrlTree> {
     return this.authService.user.pipe(
-      map(
-        (user) => {
-          const isAuth = !!user;
-          if (isAuth) {
-            return true;
-          }
-          return this.router.createUrlTree(['/auth']);
+      map((user) => {
+        const isAuth = !!user;
+        if (isAuth) {
+          return true;
         }
-        // tap((isAuth) => {
-        //   if (!isAuth) {
-        //     this.router.navigate(['/auth']);
-        //   }
-        // })
-      )
+        return this.router.createUrlTree(['/auth']);
+      })
     );
   }
 }
