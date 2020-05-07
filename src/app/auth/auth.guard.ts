69d1235f7cac48381ab7880c2eb710388c35a362
Uri <47351041+Gelelus@users.noreply.github.com>
2020-05-07 15:35:42 +0300

    added take operator

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -8,7 +8,7 @@ import {
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
-import { map } from 'rxjs/operators';
+import { map, take } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
@@ -18,6 +18,7 @@ export class AuthGuard implements CanActivate {
     router: RouterStateSnapshot
   ): boolean | Promise<boolean | UrlTree> | Observable<boolean | UrlTree> {
     return this.authService.user.pipe(
+      take(1),
       map((user) => {
         const isAuth = !!user;
         if (isAuth) {
