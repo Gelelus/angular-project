193f7d8cbd5954aefa8fd249987db6e385edfa75
Uri <47351041+Gelelus@users.noreply.github.com>
2020-05-08 15:48:38 +0300

    replaced and deleted unnessesary parts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
@@ -8,12 +8,13 @@ const appRoutes: Routes = [
     path: 'shopping-list',
     loadChildren: './shoping-list/shoping-list.module#ShopingListModule',
   },
-  { path: 'auth',
-loadChildren:'./auth/auth.module#AuthModule'}
+  { path: 'auth', loadChildren: './auth/auth.module#AuthModule' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
+  imports: [ 
+    RouterModule.forRoot(appRoutes, { preloadingStrategy: PreloadAllModules }), //preloading modules
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
-import { AuthModule } from './auth/auth.module';
+
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
