9a7a5006d791ed354c4e2f46b2cefb84766690af
Uri <47351041+Gelelus@users.noreply.github.com>
2020-05-11 10:37:40 +0300

    added ngrx to auth component

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,9 +3,12 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { catchError, tap } from 'rxjs/operators';
 import { throwError, BehaviorSubject } from 'rxjs';
-import { environment } from '../../environments/environment'
+import { environment } from '../../environments/environment';
+import { Store } from '@ngrx/store';
 
 import { User } from './user.model';
+import * as fromApp from '../store/app.reducer';
+import * as AuthActions from './store/auth.actions';
 
 export interface AuthResponseDate {
   kind: string;
@@ -19,20 +22,21 @@ export interface AuthResponseDate {
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-  user = new BehaviorSubject<User>(null);
+  // user = new BehaviorSubject<User>(null);
   private tokenExpirationTime: any;
 
-  constructor(private http: HttpClient, private router: Router) {}
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private store: Store<fromApp.AppState>
+  ) {}
 
   signup(email: string, password: string) {
     return this.http
-      .post<AuthResponseDate>(
-        environment.DataBaseUrl + 'users',
-        {
-          email: email,
-          password: password,
-        }
-      )
+      .post<AuthResponseDate>(environment.DataBaseUrl + 'users', {
+        email: email,
+        password: password,
+      })
       .pipe(
         catchError(this.handleError),
         tap((resData) => {
@@ -48,13 +52,10 @@ export class AuthService {
 
   login(email: string, password: string) {
     return this.http
-      .post<AuthResponseDate>(
-        environment.DataBaseUrl + 'users/login',
-        {
-          email: email,
-          password: password
-        }
-      )
+      .post<AuthResponseDate>(environment.DataBaseUrl + 'users/login', {
+        email: email,
+        password: password,
+      })
       .pipe(
         catchError(this.handleError),
         tap((resData) => {
@@ -75,7 +76,7 @@ export class AuthService {
       _token: string;
       _tokenExpirationDate: string;
     } = JSON.parse(localStorage.getItem('UserData'));
-    console.log(userData);
+
     if (!userData) {
       return;
     } else {
@@ -87,7 +88,15 @@ export class AuthService {
       );
 
       if (loadedUser.token) {
-        this.user.next(loadedUser);
+        // this.user.next(loadedUser);
+        this.store.dispatch(
+          new AuthActions.Login({
+            email: loadedUser.email,
+            userId: loadedUser.id,
+            token: loadedUser.token,
+            expirationDate: new Date(userData._tokenExpirationDate),
+          })
+        );
         const expirationDuration =
           new Date(userData._tokenExpirationDate).getTime() -
           new Date().getTime();
@@ -97,7 +106,8 @@ export class AuthService {
   }
 
   logout() {
-    this.user.next(null);
+    // this.user.next(null);
+    this.store.dispatch(new AuthActions.Logout());
     this.router.navigate(['/auth']);
     localStorage.removeItem('UserData');
     if (this.tokenExpirationTime) {
@@ -118,16 +128,25 @@ export class AuthService {
     expiresIn: number
   ) {
     const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
-    const user = new User(email, UserId, token, expirationDate);
-    this.user.next(user);
-    
+    // this.user.next(user);
+    const user = new User(email, UserId, token, expirationDate)
+    this.store.dispatch(
+      new AuthActions.Login({
+        email: email,
+        userId: UserId,
+        token: token,
+        expirationDate: expirationDate,
+      })
+    );
+
     this.autoLogout(expiresIn * 1000);
     localStorage.setItem('UserData', JSON.stringify(user));
   }
 
   private handleError(errorRes: HttpErrorResponse) {
+    //незабыть апнуть
     //let errorMessage = 'An error occured';
-    
+
     return throwError(errorRes.message);
   }
 }
