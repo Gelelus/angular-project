c878fdc42aaa265e1acdc4f2ff8d93c33f032dc1
Uri <47351041+Gelelus@users.noreply.github.com>
2020-05-28 12:29:48 +0300

    fixed a comment

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -11,14 +11,11 @@ import { map, take } from 'rxjs/operators';
 import { Store, select } from '@ngrx/store';
 
 import * as fromApp from '../store/app.reducer';
-import * as AuthSelectors from './store/auth.selectors'
+import * as AuthSelectors from './store/auth.selectors';
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
-  constructor(
-    private router: Router,
-    private store: Store<fromApp.AppState>
-  ) {}
+  constructor(private router: Router, private store: Store<fromApp.AppState>) {}
   canActivate(
     route: ActivatedRouteSnapshot,
     router: RouterStateSnapshot
@@ -27,8 +24,7 @@ export class AuthGuard implements CanActivate {
       select(AuthSelectors.user),
       take(1),
       map((user) => {
-        const isAuth = !!user;
-        if (isAuth) {
+        if (!!user) {
           return true;
         }
         return this.router.createUrlTree(['/auth']);
