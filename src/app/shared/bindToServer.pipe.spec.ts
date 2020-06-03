e3735f7ab61ba90e51e4e9001b142e5202d9def3
Uri <47351041+Gelelus@users.noreply.github.com>
2020-06-03 10:23:36 +0300

    fixed comments

--- src/app/shared/bindToServer.pipe.spec.ts
+++ src/app/shared/bindToServer.pipe.spec.ts
@@ -1,8 +1,8 @@
 import { BindToServerPipe } from './bindToServer.pipe';
 import { environment } from 'src/environments/environment';
 
-describe('AppComponent', () => {
-  it('should create the app', () => {
+describe('BindToServerPipe', () => {
+  it('should create string with DataBaseUrl', () => {
     const bindToServerPipe = new BindToServerPipe();
     expect(bindToServerPipe.transform('string')).toEqual(
       environment.DataBaseUrl + 'string'
